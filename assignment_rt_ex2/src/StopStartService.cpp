0e6f16be2ab73d1f2088dead3f3fb5afb92694ab
Christian <negcris02@gmail.com>
2025-03-10 21:32:14 +0000

    comments

--- assignment_rt_ex2/src/StopStartService.cpp
+++ assignment_rt_ex2/src/StopStartService.cpp
@@ -1,8 +1,30 @@
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * \file StopStartService.cpp
+ * \brief StopStartService to manage the stop and sart of the robot through service
+ * \author Christian Negri Ravera
+ * \version 0.1
+ * \date 27/02/2025
+ * 
+ * \details
+ * 
+ * Publishes to: <br>
+ *  /cmd_vel: [geometry_msg/msg/twist] <br>
+ * 
+ * Subscribes to: <br>
+ *  none <br>
+ * 
+ * \service: <br>
+ *  /stop_start: [service_msgs/srv/stop_start] <br>
+ * 
+ * Description: <br>
+ * The file implemnets the StopStartServer class to manage the service to make the robot strt or stop, based on the choice of the user.
+ * If the input to the server is true, the robot will start.
+ * If the input to the server is false, the robot will stop.
+ * If the robot will restart, the velocity assigned is obtained from the global paramters.
+ * The service publishes also the velocity of the robot
+ */
+
+
 
 #include <inttypes.h>
 #include <memory>
@@ -16,44 +38,95 @@ using std::placeholders::_1;
 using std::placeholders::_2;
 using std::placeholders::_3;
 
+
+
+/**
+ * \class StopStartServer
+ * 
+ * Description: <br>
+ * The class implements the behavious of the service which allows the user to stop or start the robot.
+ * The service is called /stop_start and it is of type StopStart.
+ */
 class StopStartServer : public rclcpp::Node
 {
 public:
-  StopStartServer()
-  : Node("stop_start_server")
+
+/**
+ * \brief StopStartServer, cosntructor of the class
+ * \param none
+ * \return none
+ * 
+ * Description: <br>
+ * The constructor creates the instances of the service and publisher of velocity and binds them to the respective variables service_ and publisher_.
+ * The variable stopped_ is initialized to false, meaning that the robot is moving.
+ */
+  StopStartServer(): Node("stop_start_server")
   {
-    service_ = this->create_service<service_msgs::srv::StopStart>("/stop_start", std::bind(&StopStartServer::handle_service, this, _1, _2, _3));
-    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    service_ = this->create_service<service_msgs::srv::StopStart>("/stop_start", std::bind(&StopStartServer::handle_service, this, _1, _2, _3));    ///< creation of the servce and binding to the service variable
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);   ///< creation of the publisher of the velocity
+    stopped_ = false;        ///< assign te value false to the var stopped: robot is moving, useful to process better the state of the robot
   }
-
 private:
-    // qui sono sempre lato server, in sto modo, sto usando il server che si trova in un nodo a se stante,
-    // il quale semplicemente vede ciò che ha nella request e lo raddoppia per la response quando viene chiamato
 
+/**
+ * \brief handle_service, function to manage the service behaviour
+ * \param request_header header of the request
+ * \param request request of the service
+ * \param response response of the service
+ * \return none
+ * 
+ * Description: <br>
+ * The function is called when the service is requested. It checks the value of the request and, based on the value of the variable stop_start, it stops or starts the robot.
+ * if the robots is stopped the velocity is set to zero, otherwise the velocity is set to value of the parameters vel_x and vel_y.
+ * The function publishes the velocity of the robot and sets the response to true.
+ */
 void handle_service(  const std::shared_ptr<rmw_request_id_t> request_header,
-        const std::shared_ptr<service_msgs::srv::StopStart::Request> request,
-        std::shared_ptr<service_msgs::srv::StopStart::Response> response)
+                      const std::shared_ptr<service_msgs::srv::StopStart::Request> request,
+                      std::shared_ptr<service_msgs::srv::StopStart::Response> response)
     {
-        
+        float vel_x = this->get_parameter("vel_x").as_double();   ///< storing the value of the parameter referred to velocity along x
+        float vel_y = this->get_parameter("vel_y").as_double();   ///< storing the value of the parameter referred to velocity along y
         geometry_msgs::msg::Twist vel;
-        if (request->stop_start)
-        {
-            vel.linear.x = 0.0;
-            vel.linear.y = 0.0;
-            vel.angular.z = 0.0;      
-        }
-        
-        publisher_->publish(vel);
-        response->success = true;
-    }
 
-    rclcpp::Service<service_msgs::srv::StopStart>::SharedPtr service_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+        
+        if (!request->stop_start){       // checking if the request is to stop the robot: velocity of the robot is set to zero
+                vel.linear.x = 0.0;
+                vel.linear.y = 0.0;
+                vel.angular.z = 0.0;
+                stopped_ = true;
+            }
+        else{                          // checking if the request is to start the robot: velocity of the robot is set to the previous value of the paramter velocity
+          if (request->stop_start){
+                vel.linear.x = vel_x;
+                vel.linear.y = vel_y;
+                stopped_ = false;
+            }
+        }
+          publisher_->publish(vel); // publishing the velocity of the robot
+          response->success = true; // setting response to true as completed correctly
+ }
+    rclcpp::Service<service_msgs::srv::StopStart>::SharedPtr service_;    ///< attribute to reference the service
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;   ///< attribute to reference publisher of velocity
+    bool stopped_;                                                        ///< attribute to reference the user choice to whether stop or start the robot
+   
 };
 
+
+/** 
+ * \brief main function
+ * \param argc number of arguments
+ * \param argv pointer to array of arguments
+ * \return always zero, since it is the main function 
+ * 
+ * Description: <br>
+ * The main function initializes the node and spins the node to keep it alive.
+ */
 int main(int argc, char * argv[])
 {
+  // initialization of the node
   rclcpp::init(argc, argv);
+
+  // Spinning the node to keep it alive and initilize the service
   rclcpp::spin(std::make_shared<StopStartServer>());
   rclcpp::shutdown();
   return 0;
--- assignment_rt_ex2/src/robot_moving.cpp
+++ assignment_rt_ex2/src/robot_moving.cpp
@@ -1,3 +1,35 @@
+/**
+ * \file robot_moving.cpp
+ * \brief this files deals with the robot movement in an S shape, given a input veloicty
+ * \author Christian Negri Ravera
+ * \version 0.1
+ * \date 27/02/2025
+ * 
+ * 
+ * \details
+ * 
+ * Publishes to: <br>
+ *  /cmd_vel: [geometry_msgs/msg/Twist] <br>
+ *  /num: [service_msgs/msg/num] <br>
+ *  /odom_feet: [nav_msgs/msg/odometry] <br>
+ * 
+ * Subscribes to: <br>
+ * /cmd_vel:  [geometry_msgs/msg/Twist] <br>
+ * /odom: [nav_msgs/msg/odometry] <br>
+ * 
+ * Services to: <br>
+ * /get_robot_posisition: [service_msgs/srv/get_robot_position] <br>
+ * 
+ * Description: <br>
+ * The file shows the implementation of the node robot_pathway: which manages the movement of the robot in an S shape, given a input velocity. <br>
+ * It is possible to set the velocity of the robot and, based on how close it is to the border of the map , it stops 
+ * Besides, some topics are exploited to obtain and publish the position and velocity of the robot.
+ * Usage of the custom message /num gives the possibility of seeing the counting going on
+ * Finally, the connection to the server gives the possibility to the user to start or stop the robot, which require the past velocity (parameters)
+ */
+
+
+
 #include <string>
 #include <chrono>
 #include <memory>
@@ -12,182 +44,172 @@
 #include "service_msgs/msg/num.hpp"
 
 using namespace std::chrono_literals;
-using GetRobotPosition = service_msgs::srv::GetRobotPosition;
+using GetRobotPosition = service_msgs::srv::GetRobotPosition; ///< alias for the service to get the robot position
 
 
-
+/**
+ * \class robot_pathway
+ * This class will deal with the robot movement and interface with the user to set the velocity
+ */
 class robot_pathway:public rclcpp::Node
 {
 public:
+	/**
+	 * \brief robot_pathway, constructor of the class
+	 * \param none 
+	 * \return none
+	 * 
+	 * Description: <br>
+	 * Initializes all publishers and subscribers, as long as the global parameters used to communicate with the server
+	 */
 	robot_pathway():Node("my_robot_pathway")
 	{
-		// initialization pulisher
-		publisher_robot_velocity_ =this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel",10);
-		// publisher per il custom msg
-		publisher_num_ = this->create_publisher<service_msgs::msg::Num>("/num",10);
-		//initialization subscriber
-		subscription_robot_position_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&robot_pathway::get_pos_callback, this, std::placeholders::_1));
-		subscription_robot_velocity_ = this->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel",10, std::bind(&robot_pathway::get_vel_callback, this, std::placeholders::_1));
-		pos_feet_robot_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("/odom_feet",10);
+		publisher_robot_velocity_ =this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel",10); 	///<initialization publisher of the velocity 
+		publisher_num_ = this->create_publisher<service_msgs::msg::Num>("/num",10); 					///<initialization publisher of the custom message increasing num 
+		pos_feet_robot_publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("/odom_feet",10); 	///<initialization publisher of the position of robot in feet
 
-		//service_ = this->create_service<service_msgs::srv::VelocitySet>("/set_velocity", std::bind(&robot_pathway::set_velocity_service, this, std::placeholders::_1,std::placeholders::_2)); // definisco callback del server
-		
-		//client_ = this->create_client<service_msgs::srv::GetRobotPosition>("/get_robot_position"); // definisco client
-		//client_vel = this->create_client<service_msgs::srv::VelocitySet>("/set_velocity");
-		
-		timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&robot_pathway::move_callback, this));
-		timer2_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&robot_pathway::num_callback, this));
-		timer3_pos_feet_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&robot_pathway::pos_robot_feet_callback, this));
-		count_ = 0;
+		subscription_robot_position_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&robot_pathway::get_pos_callback, this, std::placeholders::_1)); 		///<initialization subscription of the position
+		subscription_robot_velocity_ = this->create_subscription<geometry_msgs::msg::Twist>("/cmd_vel",10, std::bind(&robot_pathway::get_vel_callback, this, std::placeholders::_1));	///<initialization subscription of the position
 
+		timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&robot_pathway::move_callback, this)); 						///<initialization timer to move the robot each 0.5 seconds
+		timer2_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&robot_pathway::num_callback, this));						///<initialization timer to publish the number each 0.5 seconds
+		timer3_pos_feet_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&robot_pathway::pos_robot_feet_callback, this)); 	///<initialization timer to publish robot position in feet each 0.5 seconds
+		count_ = 0; 	///<initialization counter for the custom message
+
+		/**
+		 * \brief Initializes parameters to store the robot's velocity.
+		 *
+		 * Description: <br>
+		 * These parameters allow the robot to resume movement after being stopped.
+		 */
+		this -> declare_parameter ( " vel_x " , 0.0 ); 
+		this -> declare_parameter ( " vel_y " , 0.0 ); 
 	}
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // function to call the service and to load it lato client
-/*     void call_get_robot_position_service(float linear_x)
-    {
-        std::shared_ptr<GetRobotPosition::Request> request = std::make_shared<service_msgs::srv::GetRobotPosition::Request>();
-       	request->x = linear_x;
 
-        while (!client_->wait_for_service(std::chrono::seconds(1))) {
-            if (!rclcpp::ok()) {
-                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
-                return;
-            }
-            RCLCPP_INFO(this->get_logger(), "Service not available, waiting again...");
-        }
-
-        auto result = client_->async_send_request(request);
-    	if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) != rclcpp::FutureReturnCode::SUCCESS)
-  		{
-    		RCLCPP_ERROR(this->get_logger(), "service call failed :(");
- 		}
- 		 this->response_=result.get();
-  			    RCLCPP_INFO(this->get_logger(), "fatto1");		
-    }
-	// variables to store the request and the response client side
-  	std::shared_ptr<GetRobotPosition::Response> response_;
-
-
-
-	void call_set_velocity_service(float linear_x,float linear_y)
-    {
-        std::shared_ptr<service_msgs::srv::VelocitySet::Request> request = std::make_shared<service_msgs::srv::VelocitySet::Request>();
-       	request->vel_x = linear_x;
-		request->vel_y = linear_y;
-
-        while (!client_vel->wait_for_service(std::chrono::seconds(1))) {
-            if (!rclcpp::ok()) {
-                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
-                return;
-            }
-            RCLCPP_INFO(this->get_logger(), "Service not available, waiting again...");
-        }
-
-        auto result = client_vel->async_send_request(request);
-    	if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result) != rclcpp::FutureReturnCode::SUCCESS)
-  		{
-    		RCLCPP_ERROR(this->get_logger(), "service call failed :(");
- 		}
- 		 this->response2_=result.get();
- 			    RCLCPP_INFO(this->get_logger(), "fatto2");
-
-    }
-	// variables to store the request and the response client side
-  	std::shared_ptr<service_msgs::srv::VelocitySet::Response> response2_;
-
-
-	// service callback function lato server service_, function called to see whats inside the request e fill the renponse 
-	// questa funzione è una funzione di server , server side , quindi modifico sia la response che la request
-    void set_velocity_service(const std::shared_ptr<service_msgs::srv::VelocitySet::Request> request,
-                              std::shared_ptr<service_msgs::srv::VelocitySet::Response> response)
-    {
-        geometry_msgs::msg::Twist vel;
-        vel.linear.x = request->vel_x;
-        vel.linear.y = request->vel_y;
-        set_velocity(vel);
-	    RCLCPP_INFO(this->get_logger(), "Received request: vel_x=%.2f, vel_y=%.2f", vel.linear.x, vel.linear.y);
-
-        response->success = true;
-    }
- */
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-	// function to set velocty
+	/**
+	 * \brief set_velocity, function to set the velocity of the robot
+	 * \param vel [geometry_msgs::msg::Twist] velocity to be set
+	 * \return none
+	 * 
+	 * Dscription: <br>
+	 * The function obtains the velocity in input from the user and sets the velocity reference
+	 */
 	void set_velocity(geometry_msgs::msg::Twist vel){
-		//RCLCPP_INFO(this->get_logger(), "settatt velocita %f %f",vel.linear.x, vel.linear.y);
 		robot_vel_=vel;
 		publish_velocity();
-	}		
-	void publish_velocity(){
-		publisher_robot_velocity_->publish(robot_vel_);
 	}
 
-
-
-
-
+	/**
+	 * \brief publish_velocity, function to publish the velocity of the robot
+	 * \param none
+	 * \return none
+	 * 
+	 * Description: <br>
+	 * The function, when called, publishes the velocity reference to the robot topic
+	 * Stores the velocity composition x and y in parameters to ensure consistency in velocity values when the robot is stopped and restarted
+	 */		
+	void publish_velocity(){
+		publisher_robot_velocity_->publish(robot_vel_);
+        float vel_x = robot_vel_.linear.x;
+        float vel_y = robot_vel_.linear.y;
+        this->set_parameter(rclcpp::Parameter("vel_x", vel_x));
+        this->set_parameter(rclcpp::Parameter("vel_y", vel_y));
+    }
 private:
-	// movement as S shape
+	/**
+	 * \brief move_callback, function to manage the motion of the rbot in an S shape 
+	 * \param none
+	 * \return none
+	 * 
+	 * Description: <br>
+	 * The function is called every 0.5 seconds; at each iteration it checks if the position of the robot is close to the border of the map.
+	 * In case the robot is too close to the border it inverts the angular velocity to make the robot turn and do an S shape
+	 * In case the robot is not close to the border of the map, it keeps moving with the same velocity
+	 * If the border is overpassed the robot stops
+	 */
 	void move_callback(){
-		geometry_msgs::msg::Twist final_vel;
-		float radius = 0.7;
-
-
-	if (pos_x_ > 8 - radius) {
+	geometry_msgs::msg::Twist final_vel;
+	float radius = 0.7;		///< radius of the curve
+	if (pos_x_ > 8 - radius) { 							// right border
 	    final_vel.linear.x = robot_vel_.linear.x;  
 	    final_vel.angular.z = robot_vel_.linear.x / radius; 
-	} else if (pos_x_ < -8 + radius) {
+	} 
+	else if (pos_x_ < -8 + radius) { 					// left border
 	    final_vel.linear.x = robot_vel_.linear.x;
 	    final_vel.angular.z = -robot_vel_.linear.x / radius;  
-	} else if (pos_y_ > 8 - radius) {
+	}
+	else if (pos_y_ > 8 - radius) { 					// upper border
 	    final_vel.linear.x = robot_vel_.linear.x;  
 	    final_vel.angular.z = robot_vel_.linear.x / radius;  
-	} else if (pos_y_ < -8 + radius) {
+	} 
+	else if (pos_y_ < -8 + radius) { 					// lower border
 	    final_vel.linear.x = robot_vel_.linear.x;  
 	    final_vel.angular.z = -robot_vel_.linear.x / radius;  
-	} else if ((pos_y_ >= 10 && pos_y_ <= 13) || 
+	}
+	else if ((pos_y_ >= 10 && pos_y_ <= 13) || 			// overpassed border
 		   (pos_y_ <= -10 && pos_y_ >= -13) || 
 		   (pos_x_ >= 13 && pos_x_ <= 16) || 
-		   (pos_x_ <= -13 && pos_x_ >= -16)) {
+		   (pos_x_ <= -13 && pos_x_ >= -16)) { 			
 	    final_vel.linear.x = 0;
 	    final_vel.linear.y = 0;
-	} else {
+	} 
+	else {												// inside the boredrs
 	    final_vel.linear.x = robot_vel_.linear.x;
 	    final_vel.angular.z = 0.0;  
 	}
-		
-		
-
 		robot_vel_ = final_vel;
 		publish_velocity();
 	}
 
+	/**
+	 * \brief num_callback, function to publish the cussom message with the count
+	 * \param none
+	 * \return none
+	 * 
+	 * Description: <br>
+	 * The function in called every 0.5 seconds and assigns the value of the count to the custom message nd published it
+	 */
 	void num_callback()
 	{
-		auto message = service_msgs::msg::Num();                               // CHANGE
-		message.num = this->count_++;                                        // CHANGE
+		auto message = service_msgs::msg::Num();                             
+		message.num = this->count_++;                                        
 		publisher_num_->publish(message);
 	}
 
-	
-
-
-
-	// callback for position
+	/**
+	 *  \brief get_pos_callback, function to obtain the position of the robot
+	 * 	\param none
+	 * 	\return none
+	 * 
+	 * Description: <br>
+	 * The function in called every 0.5 seconds and assigns to the coordinate reference variables x and y the position of the robot
+	 */
 	void get_pos_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
 
 		pos_x_= msg->pose.pose.position.x;
 		pos_y_= msg->pose.pose.position.y;
 	}
-	// callback for velocity
-	void get_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg){
 
+	/**
+	 *  \brief get_vel_callback, function to obtain the velocity of the robot
+	 * 	\param none
+	 * 	\return none
+	 * 
+	 * Description: <br>
+	 * The function in called every 0.5 seconds and assigns to the velocity reference variable the velocity of the robot
+	 */
+	void get_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg){
 		robot_vel_ = *msg;
 	}
 
-
+	/**
+	 *  \brief pos_robot_feet_callback, function publishes the position of the robot in feet
+	 * 	\param none
+	 * 	\return none
+	 * 
+	 * Description: <br>
+	 * The function in called every 0.5 seconds and publishes the value of the reference position variables converted in feet
+	 */
 	void pos_robot_feet_callback(){
 		nav_msgs::msg::Odometry msg;
 		msg.pose.pose.position.x = pos_x_*3.28;
@@ -197,57 +219,52 @@ private:
 
 
 
-
-
-
-	// attributes
-	rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_robot_position_;
-	rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_robot_velocity_;
-	rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_robot_velocity_;
-	rclcpp::Publisher<service_msgs::msg::Num>::SharedPtr publisher_num_;
-	/* rclcpp::Service<service_msgs::srv::VelocitySet>::SharedPtr service_; // definition of the server for the service set_velocity
-	rclcpp::Client<service_msgs::srv::GetRobotPosition>::SharedPtr client_; // definition of the client fot the service get_robot_position
-	rclcpp::Client<service_msgs::srv::VelocitySet>::SharedPtr client_vel;
- */
-
-
-	// publisher for feet position
-	rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pos_feet_robot_publisher_;
-
-
-	float pos_x_;
-	float pos_y_;
-	geometry_msgs::msg::Twist robot_vel_;
-	rclcpp::TimerBase::SharedPtr timer_;
-	rclcpp::TimerBase::SharedPtr timer2_;
-	rclcpp::TimerBase::SharedPtr timer3_pos_feet_;
-	size_t count_;
-
+	rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_robot_position_; 		///< attribute to subscribe to position
+	rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_robot_velocity_;	///< attribute to subscribe to velocity
+	rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_robot_velocity_; 			///< attribute to publish to position
+	rclcpp::Publisher<service_msgs::msg::Num>::SharedPtr publisher_num_; 						///< attribute to publish custom message
+	rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pos_feet_robot_publisher_;			///< attribute to publish to position in feet
+	float pos_x_;																				///< attribute to reference coordinate x
+	float pos_y_;																				///< attribute to reference coordinate y									
+	geometry_msgs::msg::Twist robot_vel_;														///< attribute to reference of the velocity of the robot
+	rclcpp::TimerBase::SharedPtr timer_;														///< attribute to timer for motion
+	rclcpp::TimerBase::SharedPtr timer2_;														///< attribute to timer for number 
+	rclcpp::TimerBase::SharedPtr timer3_pos_feet_;												///< attribute to timer for position in feet
+	size_t count_;																				///< attribute to count for custom message 	
 
 };
-int tempo=0;
+
+
+/**
+* \brief main function.
+* \param argc num of input argument if any.
+* \param argv pointer to array of arguments if any.
+* \return always zero since it is the main function.
+*
+* Description: <br>
+* The main function initializes the node and.
+* Asks the user to input the linear velocities along x and y and sets the velocity of the robot.
+* Spins the node to keep it alive.
+*/
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc,argv);
-  auto node = std::make_shared<robot_pathway>();
+	// initialization of the node
+	rclcpp::init(argc,argv);
+	// initialize the class robot_pathway
+	auto node = std::make_shared<robot_pathway>();
 
-  geometry_msgs::msg::Twist vel_input ;
-  std::cout<< "give linear velocity along x"<< std::endl;std::cin >> vel_input.linear.x;
-  std::cout<< "give linear velocity along y"<< std::endl;std::cin >> vel_input.linear.y;
-  node->set_velocity(vel_input);
-	RCLCPP_INFO(node->get_logger(), "tempo: %d", tempo);
+	// asking the user for the velocity and setting the velocity of the robot
+	geometry_msgs::msg::Twist vel_input ;		
+	std::cout<< "give linear velocity along x"<< std::endl;std::cin >> vel_input.linear.x; 
+	std::cout<< "give linear velocity along y"<< std::endl;std::cin >> vel_input.linear.y;
+	node->set_velocity(vel_input); 
 
-	
-	//if(tempo>20){ 
-	/* node->call_set_velocity_service(vel_input.linear.x*10,vel_input.linear.y*10);
-	node->call_get_robot_position_service(vel_input.linear.x); */
-	//}
-	tempo++;	
-  rclcpp::spin(node);
-  rclcpp::shutdown();
+	// spinning the node
+  	rclcpp::spin(node);
+  	rclcpp::shutdown();
   
   
   
-  return 0;
+  	return 0;
 }
 
